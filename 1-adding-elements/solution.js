febfdd58fa4ee862f201d96ceaebfac5d602d308
David Unger <davidunger@outlook.com>
2021-03-19 09:23:13 +0100

    Execise 3 half way done

--- 1-adding-elements/solution.js
+++ 1-adding-elements/solution.js
@@ -1,6 +1,8 @@
 const button = document.querySelector("button");
+const container = document.querySelector(".container");
 
 button.addEventListener("click", () => {
   const boxElement = document.createElement("div");
   boxElement.classList.add("box");
+  container.append(boxElement);
 });
--- 2-removing-elements/solution.js
+++ 2-removing-elements/solution.js
@@ -1,13 +1,17 @@
 const button = document.querySelector("button");
 const container = document.querySelector(".container");
+let confirmation;
 
 button.addEventListener("click", () => {
   const boxElement = document.createElement("div");
   boxElement.classList.add("box");
   container.append(boxElement);
   boxElement.addEventListener("click", () => {
-    boxElement.remove();
+    confirmation = confirm("Do you really want to proceed?");
+    if (confirmation === true) {
+      boxElement.remove();
+    } else {
+      alert("Cancelled");
+    }
   });
 });
-
-const h1 = document.createElement("h1");
